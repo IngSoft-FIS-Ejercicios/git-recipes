c81d0c23dd8f38e63edd15113b492b85e6bcbb61
Lucas Barnett <lucasbarnett@192.168.1.12>
2026-09-08 14:46:32 -0300

    agregue descripcion

--- src/domain/recipe.ts
+++ src/domain/recipe.ts
@@ -1,9 +1,11 @@
 export class Recipe {
   _name!: string;
   _category?: string;
+  _descripcion?: string; 
 
   constructor(aName: string) {
     this.name = aName;
+  
   }
 
   get name(): string {
@@ -11,14 +13,25 @@ export class Recipe {
   }
 
   set name(aName: string) {
-    const trimmed = aName.trim();
+    const trimmed = aName.trim(); 
+    
     if (trimmed.length === 0) {
       throw new Error("El nombre de la receta no puede ser vacío.");
     }
     this._name = trimmed;
   }
+  
 
-  get category(): string | undefined {
+ 
+get descripcion(): string{
+  return this._descripcion || "Sin Descripcion";
+}
+
+set descripcion(adesc: string) {
+  this._descripcion = adesc; 
+}
+
+ get category(): string | undefined {
     return this._category;
   }
 
@@ -27,6 +40,6 @@ export class Recipe {
   }
 
   toString(): string {
-    return `Receta: ${this.name} - categoría: ${this.category}`;
+    return `Receta: ${this.name} - categoría: ${this.category} - Descripción: ${this.descripcion}`;
   }
 }
